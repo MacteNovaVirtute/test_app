d6136defbb4272695d2e6238d486a8f4f45cc6fa
MacteNovaVirtute <77696865+MacteNovaVirtute@users.noreply.github.com>
2021-05-19 11:22:53 +0300

    Added counter

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatefulWidget{
 class _MyAppState extends State<MyApp> {
 
   Color _backgroundColor = Colors.white;
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -28,18 +29,27 @@ class _MyAppState extends State<MyApp> {
       });
     }
 
+    void incrementCounter() {
+      setState(() {
+        _counter++;
+      });
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: GestureDetector(
-        onTap: changeBackground,
+        onTap: (){
+          changeBackground();
+          incrementCounter();
+        },
         child:Scaffold(
-        appBar: AppBar(
-        title: Text("Нажмите на любую часть экрана",
-        style: TextStyle(fontFamily: 'Appetite',
-        fontSize: 22.0
-        ),
-      ),
-    ),
+          appBar: AppBar(
+            title: Text("Number of clicks: $_counter",
+              style: TextStyle(fontFamily: 'Appetite',
+                  fontSize: 22.0
+              ),
+            ),
+          ),
           backgroundColor: _backgroundColor,
           body: Center(
               child: Text(
